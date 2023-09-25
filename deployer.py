2e18cf327fdeefe951c760e3aa46234500789a00
Naveed <naveedinno@proton.me>
2023-09-25 10:43:44 +0200

    Add symmioVersion to configs

--- deployer.py
+++ deployer.py
@@ -1,7 +1,27 @@
 import json
 import os
+import shutil
 import subprocess
 import sys
+
+
+def copy_abi_files(subgraph_dir, version):
+    """
+    Copy the ABI files for the provided version from the root 'abis' directory
+    to the 'abis' directory inside the given subgraph directory.
+    """
+    root_abis_dir = "abis"
+    subgraph_abis_dir = os.path.join(subgraph_dir, "abis")
+
+    files_to_copy = [f"symmio_{version}.json", f"symmioMultiAccount_{version}.json"]
+
+    for file_name in files_to_copy:
+        source = os.path.join(root_abis_dir, file_name)
+        destination = os.path.join(
+            subgraph_abis_dir, file_name.replace(f"_{version}", "")
+        )
+
+        shutil.copy(source, destination)
 
 
 def deploy_subgraph(subgraph_dir, config_file, deploy_url):
@@ -15,18 +35,29 @@ def deploy_subgraph(subgraph_dir, config_file, deploy_url):
 
     # Prepare the subgraph
     if subgraph_dir == "analytics":
-        subprocess.run(["mustache", adjusted_config_path, "template.yaml"], check=True,
-                       stdout=open("subgraph.yaml", "w"))
-        subprocess.run(["mustache", adjusted_config_path, "src/contract_utils.template.ts"], check=True,
-                       stdout=open("src/contract_utils.ts", "w"))
+        subprocess.run(
+            ["mustache", adjusted_config_path, "template.yaml"],
+            check=True,
+            stdout=open("subgraph.yaml", "w"),
+        )
+        subprocess.run(
+            ["mustache", adjusted_config_path, "src/contract_utils.template.ts"],
+            check=True,
+            stdout=open("src/contract_utils.ts", "w"),
+        )
     else:
-        subprocess.run(["mustache", adjusted_config_path, "template.yaml"], check=True,
-                       stdout=open("subgraph.yaml", "w"))
+        subprocess.run(
+            ["mustache", adjusted_config_path, "template.yaml"],
+            check=True,
+            stdout=open("subgraph.yaml", "w"),
+        )
 
     subprocess.run(["graph", "build"], check=True)
 
     # Deploy the subgraph
-    subprocess.run(["graph", "deploy", "--product", "hosted-service", deploy_url], check=True)
+    subprocess.run(
+        ["graph", "deploy", "--product", "hosted-service", deploy_url], check=True
+    )
 
     # Change directory back to the root directory
     os.chdir("..")
@@ -34,7 +65,9 @@ def deploy_subgraph(subgraph_dir, config_file, deploy_url):
 
 def main():
     if len(sys.argv) < 2:
-        print("Please provide the full path to the configuration file (e.g., configs/fix_review_test.json)")
+        print(
+            "Please provide the full path to the configuration file (e.g., configs/fix_review_test.json)"
+        )
         sys.exit(1)
 
     config_file = sys.argv[1]
@@ -48,11 +81,15 @@ def main():
     with open(config_file, "r") as f:
         config = json.load(f)
 
-    deploy_subgraph("analytics", config_file, config["analyticsDeployUrl"])
-    deploy_subgraph("main", config_file, config["mainDeployUrl"])
-    deploy_subgraph("parties", config_file, config["partiesDeployUrl"])
+    for subgraph in ["analytics", "main", "parties"]:
+        # Copy the ABI files to each subgraph directory
+        copy_abi_files(subgraph, config["symmioVersion"])
 
-    print("All subgraphs have been prepared and deployed!")
+        deploy_subgraph(subgraph, config_file, config[f"{subgraph}DeployUrl"])
+
+        print(f"{subgraph} subgraph deployed")
+
+    print("Done")
 
 
 if __name__ == "__main__":
