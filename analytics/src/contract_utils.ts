7db38b430747a790fd5f0a5283cdf4861ea710a9
Navid <mojtaba.farokhi99@gmail.com>
2023-10-27 16:18:36 +0330

    Add PartyALiquidation/PartyALiquidationDisputed

--- analytics/src/contract_utils.ts
+++ analytics/src/contract_utils.ts
@@ -1,8 +1,18 @@
 import {Address, BigInt} from "@graphprotocol/graph-ts";
-import {symmio, symmio__getQuoteResultValue0Struct} from "../generated/symmio/symmio";
+import {
+	symmio,
+	symmio__getLiquidatedStateOfPartyAResultValue0Struct,
+	symmio__getQuoteResultValue0Struct
+} from "../generated/symmio/symmio";
 
 export function getQuote(address: Address, id: BigInt): symmio__getQuoteResultValue0Struct | null {
 	const contract = symmio.bind(address);
 	let result = contract.try_getQuote(id);
+	return result.reverted ? null : result.value;
+}
+
+export function getLiquidatedStateOfPartyA(address: Address, partyA: Address): symmio__getLiquidatedStateOfPartyAResultValue0Struct | null {
+	const contract = symmio.bind(address);
+	let result = contract.try_getLiquidatedStateOfPartyA(partyA);
 	return result.reverted ? null : result.value;
 }
