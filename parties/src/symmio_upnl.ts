852e7fabf6540076fb470b11d8c448556138edf0
zigBalthazar <zigbalthazar@gmail.com>
2023-11-14 19:44:17 +0330

    feat: add balanceInfo of parties in handleLiquidateParties.

--- parties/src/symmio_upnl.ts
+++ parties/src/symmio_upnl.ts
@@ -6,6 +6,7 @@ import {
     DeallocatePartyA as DeallocatePartyAEvent,
     LiquidatePartyA as LiquidatePartyAEvent,
     LiquidatePartyB as LiquidatePartyBEvent,
+    symmio as symmio
 } from "../generated/symmio/symmio"
 import { ResultPartyA, ResultPartyB, } from "../generated/schema"
 
@@ -120,18 +121,29 @@ export function handleLiquidatePartyA(
 ): void {
     let entity = ResultPartyA.load(event.params.partyA.toHex())
     if (entity) {
+        const balanceInfoOfPartyA = symmio.bind(event.address).balanceInfoOfPartyA(event.params.partyA)
+
         entity.liquidatePartyATimeStamp = event.block.timestamp
         entity.trHashLiquidate = event.transaction.hash
+        entity.allocatedBalance = balanceInfoOfPartyA.value0
+        entity.cva = balanceInfoOfPartyA.value1
+        entity.lf = balanceInfoOfPartyA.value3
         entity.save()
     }
-
 }
 
 export function handleLiquidatePartyB(event: LiquidatePartyBEvent): void {
     let entity = ResultPartyB.load(event.params.partyA.toHex() + '-' + event.params.partyB.toHex())
     if (entity) {
+        const balanceInfoOfPartyB= symmio.bind(event.address).balanceInfoOfPartyB(event.params.partyB,event.params.partyA)
+
         entity.liquidatePartyBTimeStamp = event.block.timestamp
         entity.trHashLiquidate = event.transaction.hash
+        entity.allocatedBalance = balanceInfoOfPartyB.value0
+        entity.cva = balanceInfoOfPartyB.value1
+        entity.lf = balanceInfoOfPartyB.value3
         entity.save()
     }
 }
+
+
