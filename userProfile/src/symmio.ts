35792bc019a46eb21f0c8c9c06359429b28082a1
Naveed <naveedinno@proton.me>
2024-01-25 14:05:31 +0200

    Fix minor problem

--- userProfile/src/symmio.ts
+++ userProfile/src/symmio.ts
@@ -234,6 +234,7 @@ export function handleSendQuote(event: SendQuote): void {
     quote.avgClosedPrice = BigInt.zero()
     quote.pnl = BigInt.zero()
     quote.fundingPaid = BigInt.zero()
+    quote.fundingReceived = BigInt.zero()
     quote.collateral = getConfiguration(event).collateral
     quote.save()
 
