57bea088e2d497fd714f539c7b673f0816b02e50
zxsimba <x@x.x>
2024-03-17 10:29:27 +0330

    deploy events subgraph in dev env

--- confgen.py
+++ confgen.py
@@ -62,7 +62,7 @@ def main():
     with open(config_file, "r") as f:
         config = json.load(f)
 
-    for subgraph in ["main", "parties", "fundingrate"]:
+    for subgraph in ["main", "parties", "fundingrate", "events"]:
         # Copy the ABI files to each subgraph directory
         copy_abi_files(subgraph, config["symmioVersion"])
 
