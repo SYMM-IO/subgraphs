fa51bf3686d5fb140e01c7b01ab0c77b585a329b
kindtechie <kindtechie@gmail.com>
2024-04-12 10:34:33 +0200

    fix manager scripts and add function global counter to helper

---
+++ UniGraph/quotes/handlers/SendQuoteHandler.ts
@@ -0,0 +1,14 @@
+
+import {SendQuoteHandler as CommonSendQuoteHandler} from "../../common/handlers/SendQuoteHandler"
+import {SendQuote} from "../../generated/symmio/symmio"
+
+export class SendQuoteHandler extends CommonSendQuoteHandler {
+
+    constructor(event: SendQuote) {
+        super(event)
+    }
+
+    handle(): void {
+		super.handle()
+    }
+}
