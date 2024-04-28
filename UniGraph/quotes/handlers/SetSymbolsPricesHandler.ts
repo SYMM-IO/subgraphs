50836e94ff6d4c428b99cc07628cd827f8a342c3
kindtechie <kindtechie@gmail.com>
2024-04-28 10:23:40 +0200

    add set symbol price

---
+++ UniGraph/quotes/handlers/SetSymbolsPricesHandler.ts
@@ -0,0 +1,34 @@
+
+import { SetSymbolsPricesHandler as CommoneSetSymbolsPricesHandler } from "../../common/handlers/SetSymbolsPricesHandler"
+import { getGlobalCounterAndInc } from "../../common/helper"
+import { PartyASymbolPrice } from "../../generated/schema"
+import { SetSymbolsPrices } from "../../generated/symmio/symmio"
+
+export class SetSymbolsPricesHandler extends CommoneSetSymbolsPricesHandler {
+
+  constructor(event: SetSymbolsPrices) {
+    super(event)
+  }
+
+  handle(): void {
+    super.handle()
+    super.handleQuote()
+
+
+    const listOFSymbols = this.event.params.symbolIds.slice(0)
+    const listOfPrices = this.event.params.prices.slice(0)
+    for (let i = 0, lenList = listOFSymbols.length; i < lenList; i++) {
+      let partyASP = PartyASymbolPrice.load(this.event.params.partyA.toHexString().concat('-').concat(listOFSymbols[i].toHex()))
+      if (!partyASP) {
+        partyASP = new PartyASymbolPrice(this.event.params.partyA.toHexString().concat('-').concat(listOFSymbols[i].toHex()))
+      }
+      partyASP.symbolId = listOFSymbols[i]
+      partyASP.partyA = this.event.params.partyA
+      partyASP.requestedOpenPrice = listOfPrices[i]
+      partyASP.timeStamp = this.event.block.timestamp
+      partyASP.trHash = this.event.transaction.hash
+      partyASP.GlobalCounter = getGlobalCounterAndInc()
+      partyASP.save()
+    }
+  }
+}
