30e338d4b7cdeb55f7b4340bccdec6481b22ee4b
kindtechie <kindtechie@gmail.com>
2024-06-01 09:27:32 +0330

    add some handler

--- UniGraph/user_profile/handlers/LiquidatePositionsPartyAHandler.ts
+++ UniGraph/user_profile/handlers/LiquidatePositionsPartyAHandler.ts
@@ -1,5 +1,6 @@
 import { LiquidatePositionsPartyAHandler as CommonLiquidatePositionsPartyAHandler } from "../../common/handlers/LiquidatePositionsPartyAHandler"
 import { LiquidatePositionsPartyA } from "../../generated/symmio/symmio"
+import { handleLiquidatePosition } from "./handleLiquidatePosition"
 
 export class LiquidatePositionsPartyAHandler extends CommonLiquidatePositionsPartyAHandler {
 
@@ -17,7 +18,7 @@ export class LiquidatePositionsPartyAHandler extends CommonLiquidatePositionsPar
 		let event = super.getEvent()
 		for (let i = 0; i < event.params.quoteIds.length; i++) {
 			const qId = event.params.quoteIds[i]
-			handleLiquidatePosition(event, qId)
+			handleLiquidatePosition(event, qId, "LiquidatePositionsPartyA")
 		}
 	}
 }
