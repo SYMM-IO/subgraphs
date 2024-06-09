f83c04731f3d0cd60c9124a7984bc0e7d8dcd54d
kindtechie <kindtechie@gmail.com>
2024-06-09 09:38:01 +0200

    fix some bug

--- common/handlers/ChargeFundingRateHandler.ts
+++ common/handlers/ChargeFundingRateHandler.ts
@@ -1,8 +1,5 @@
 import { BaseHandler } from "./BaseHandler"
 import { ChargeFundingRate } from "../../generated/symmio/symmio"
-import { setEventTimestampAndTransactionHashAndAction } from "../utils/quote&analitics&user"
-import { Quote } from "../../generated/schema"
-import { FACTOR, getGlobalCounterAndInc } from "../utils"
 
 export class ChargeFundingRateHandler extends BaseHandler {
 	protected event: ChargeFundingRate
@@ -20,21 +17,5 @@ export class ChargeFundingRateHandler extends BaseHandler {
 	}
 
 	handleQuote(): void {
-		for (let i = 0, lenQ = this.event.params.quoteIds.length; i < lenQ; i++) {
-			let qoutId = this.event.params.quoteIds[i]
-			let quote = Quote.load(qoutId.toString())!
-			quote.globalCounter = getGlobalCounterAndInc()
-			quote.blockNumber = this.event.block.number
-			if (quote.positionType) { // SHORT position
-				quote.openedPrice = quote.openedPrice!.minus(quote.openedPrice!.times(this.event.params.rates[i]).div(FACTOR))
-			} else {
-				quote.openedPrice = quote.openedPrice!.plus(quote.openedPrice!.times(this.event.params.rates[i]).div(FACTOR))
-			}
-			quote.timeStamp = this.event.block.timestamp
-			quote.save()
-			setEventTimestampAndTransactionHashAndAction(quote.eventsTimestamp, this.event.block.timestamp,
-				'ChargeFundingRate', this.event.transaction.hash)
-		}
-
 	}
 }
