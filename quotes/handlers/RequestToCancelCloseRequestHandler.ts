741689974e4aa3b9e0d70ecfc7f544149eec8d7a
kindtechie <kindtechie@gmail.com>
2024-06-10 13:20:17 +0200

    create quote in accept cancle request

--- quotes/handlers/RequestToCancelCloseRequestHandler.ts
+++ quotes/handlers/RequestToCancelCloseRequestHandler.ts
@@ -1,6 +1,7 @@
 import {
 	RequestToCancelCloseRequestHandler as CommonRequestToCancelCloseRequestHandler,
 } from "../../common/handlers/RequestToCancelCloseRequestHandler"
+import { removeQuoteFromPendingList } from "../../common/utils/quote"
 import { RequestToCancelCloseRequest } from "../../generated/symmio/symmio"
 
 export class RequestToCancelCloseRequestHandler extends CommonRequestToCancelCloseRequestHandler {
@@ -12,5 +13,9 @@ export class RequestToCancelCloseRequestHandler extends CommonRequestToCancelClo
 	handle(): void {
 		super.handle()
 		super.handleQuote()
+		let event = super.getEvent()
+		if (event.params.quoteStatus === 3) {
+			removeQuoteFromPendingList(event.params.quoteId)
+		}
 	}
 }
