3bd73ca892e0372939917b9b75fda0eaee82043b
kindtechie <kindtechie@gmail.com>
2024-06-10 16:50:17 +0330

    create quote in accept cancle request

--- quotes/handlers/AcceptCancelRequestHandler.ts
+++ quotes/handlers/AcceptCancelRequestHandler.ts
@@ -1,6 +1,7 @@
 import { AcceptCancelRequestHandler as CommonAcceptCancelRequestHandler } from "../../common/handlers/AcceptCancelRequestHandler"
-import { getGlobalCounterAndInc } from "../../common/utils"
+import { getGlobalCounterAndInc, getQuote, symbolIdToSymbolName } from "../../common/utils"
 import { removeQuoteFromPendingList } from "../../common/utils/quote"
+import { initialHelper, setEventTimestampAndTransactionHashAndAction } from "../../common/utils/quote&analitics&user"
 
 import { PartyA, PartyBPartyA, Quote } from "../../generated/schema"
 import { AcceptCancelRequest } from "../../generated/symmio/symmio"
@@ -16,5 +17,26 @@ export class AcceptCancelRequestHandler extends CommonAcceptCancelRequestHandler
 		super.handleQuote()
 		let event = super.getEvent()
 		removeQuoteFromPendingList(event.params.quoteId)
+		const quoteStr = event.params.quoteId.toString()
+		let quote = Quote.load(quoteStr)
+		if (!quote) {
+			quote = new Quote(quoteStr)
+			quote.globalCounter = getGlobalCounterAndInc()
+			quote.quoteId = event.params.quoteId
+			quote.quoteStatus = event.params.quoteStatus
+			quote.timeStamp = event.block.timestamp
+
+			const quoteData = getQuote(quote.quoteId, event.address)
+			let initialquote = initialHelper(quoteData)
+			const symbol = symbolIdToSymbolName(initialquote.symbolId, event.address)
+			initialquote.symbol = symbol
+			initialquote.save()
+			quote.partyA = initialquote.partyA
+			quote.tradingFee = initialquote.tradingFee!
+			quote.initialData = initialquote.id
+			quote.save()
+			setEventTimestampAndTransactionHashAndAction(quoteStr, this.event.block.timestamp,
+				'AcceptCancelRequest', this.event.transaction.hash)
+		}
 	}
 }
