a92a757fb716cc3a0e4a4421a57b146e7794cb1a
naveed <naveedinno@proton.me>
2024-06-13 17:39:21 +0330

    Add addSymbolHandler to analytics

---
+++ analytics/handlers/AddSymbolHandler.ts
@@ -0,0 +1,27 @@
+import { AddSymbolHandler as CommonAddSymbolHandler } from "../../common/handlers/AddSymbolHandler"
+import { Symbol } from "../../generated/schema"
+import { AddSymbol } from "../../generated/symmio/symmio"
+
+export class AddSymbolHandler extends CommonAddSymbolHandler {
+
+    constructor(event: AddSymbol) {
+        super(event)
+    }
+
+    handle(): void {
+        super.handle()
+        const globalCounter = super.handleGlobalCounter()
+        super.handleQuote()
+        super.handleSymbol()
+        super.handleUser()
+        super.handleAccount()
+
+        let event = super.getEvent()
+        let symbol = new Symbol(event.params.id.toString())
+        symbol.name = event.params.name
+        symbol.tradingFee = event.params.tradingFee
+        symbol.timestamp = event.block.timestamp
+        symbol.updateTimestamp = event.block.timestamp
+        symbol.save()
+    }
+}
