41a9bb01d6ae7fef14509b2ba7f221175b9520ad
naveed <naveedinno@proton.me>
2024-06-15 14:19:26 +0200

    Fix minor problems

--- analytics/handlers/AddSymbolHandler.ts
+++ analytics/handlers/AddSymbolHandler.ts
@@ -1,27 +1,16 @@
 import { AddSymbolHandler as CommonAddSymbolHandler } from "../../common/handlers/AddSymbolHandler"
-import { Symbol } from "../../generated/schema"
 import { AddSymbol } from "../../generated/symmio/symmio"
 
 export class AddSymbolHandler extends CommonAddSymbolHandler {
+	constructor(event: AddSymbol) {
+		super(event)
+	}
 
-    constructor(event: AddSymbol) {
-        super(event)
-    }
-
-    handle(): void {
-        super.handle()
-        const globalCounter = super.handleGlobalCounter()
-        super.handleQuote()
-        super.handleSymbol()
-        super.handleUser()
-        super.handleAccount()
-
-        let event = super.getEvent()
-        let symbol = new Symbol(event.params.id.toString())
-        symbol.name = event.params.name
-        symbol.tradingFee = event.params.tradingFee
-        symbol.timestamp = event.block.timestamp
-        symbol.updateTimestamp = event.block.timestamp
-        symbol.save()
-    }
+	handle(): void {
+		super.handle()
+		super.handleQuote()
+		super.handleSymbol()
+		super.handleUser()
+		super.handleAccount()
+	}
 }
--- common/handlers/AddSymbolHandler.ts
+++ common/handlers/AddSymbolHandler.ts
@@ -1,5 +1,6 @@
 import { BaseHandler } from "./BaseHandler"
 import { AddSymbol } from "../../generated/symmio/symmio"
+import { Symbol } from "../../generated/schema"
 
 export class AddSymbolHandler extends BaseHandler {
 	protected event: AddSymbol
@@ -8,16 +9,23 @@ export class AddSymbolHandler extends BaseHandler {
 		super(event)
 		this.event = event
 	}
-	
+
 	protected getEvent(): AddSymbol {
 		return this.event
 	}
 
 	handle(): void {
+		const globalCounter = super.handleGlobalCounter()
+		let event = this.getEvent()
+		let symbol = new Symbol(event.params.id.toString())
+		symbol.globalCounter = globalCounter
+		symbol.name = event.params.name
+		symbol.tradingFee = event.params.tradingFee
+		symbol.timestamp = event.block.timestamp
+		symbol.updateTimestamp = event.block.timestamp
+		symbol.blockNumber = event.block.number
+		symbol.save()
 	}
 
-	handleQuote(): void {
-		// TODO
-	}
+	handleQuote(): void {}
 }
-
