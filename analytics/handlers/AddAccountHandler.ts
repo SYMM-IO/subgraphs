a79000b66f877048f556772e9ca3d048450c71d5
kindtechie <kindtechie@gmail.com>
2024-06-18 10:53:29 +0330

    fix add account mutli account

--- analytics/handlers/AddAccountHandler.ts
+++ analytics/handlers/AddAccountHandler.ts
@@ -13,18 +13,6 @@ export class AddAccountHandler extends CommonAddAccountHandler {
 
 	handle(): void {
 		super.handle()
-		let event = super.getEvent()
-		let user = User.load(event.params.user.toHexString())
-		if (user == null) {
-			user = createNewUser(event.params.user, event.block, event.transaction)
-		} else {
-			const dh = getDailyHistoryForTimestamp(event.block.timestamp, event.address)
-			dh.newUsers = dh.newUsers.plus(BigInt.fromString("1"))
-			dh.save()
-			const th = getTotalHistory(event.block.timestamp, event.address)
-			th.users = th.users.plus(BigInt.fromString("1"))
-			th.save()
-		}
-		createNewAccount(event.params.account, user, event.address, event.block, event.transaction, event.params.name)
+
 	}
 }
