11f680f1052df2ef8cf320c38ea89d5de58c5e15
kindtechie <kindtechie@gmail.com>
2024-06-18 10:18:52 +0330

    fix add account

--- analytics/handlers/AddAccountHandler.ts
+++ analytics/handlers/AddAccountHandler.ts
@@ -1,6 +1,9 @@
+import { BigInt } from "@graphprotocol/graph-ts"
 import { AddAccountHandler as CommonAddAccountHandler } from "../../common/handlers/AddAccountHandlerMultiAccount"
+import { createNewAccount, createNewUser } from "../../common/utils/analytics&user_profile"
 import { AddAccount } from "../../generated/symmioMultiAccount_0/symmioMultiAccount"
-import { newUserAndAccount } from "../utils"
+import { getDailyHistoryForTimestamp, getTotalHistory, newUserAndAccount } from "../utils"
+import { User } from "../../generated/schema"
 
 export class AddAccountHandler extends CommonAddAccountHandler {
 
@@ -11,6 +14,17 @@ export class AddAccountHandler extends CommonAddAccountHandler {
 	handle(): void {
 		super.handle()
 		let event = super.getEvent()
-		newUserAndAccount(event.params.user, event.block, event.transaction)
+		let user = User.load(event.params.user.toHexString())
+		if (user == null) {
+			user = createNewUser(event.params.user, event.block, event.transaction)
+		} else {
+			const dh = getDailyHistoryForTimestamp(event.block.timestamp, event.address)
+			dh.newUsers = dh.newUsers.plus(BigInt.fromString("1"))
+			dh.save()
+			const th = getTotalHistory(event.block.timestamp, event.address)
+			th.users = th.users.plus(BigInt.fromString("1"))
+			th.save()
+		}
+		createNewAccount(event.params.account, user, event.address, event.block, event.transaction, event.params.name)
 	}
 }
