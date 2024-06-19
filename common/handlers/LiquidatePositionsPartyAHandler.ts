6c71bb93d618e848c365f98e079c3845b90e8d5b
kindtechie <kindtechie@gmail.com>
2024-06-19 05:47:30 +0200

    fix some bug

--- common/handlers/LiquidatePositionsPartyAHandler.ts
+++ common/handlers/LiquidatePositionsPartyAHandler.ts
@@ -1,5 +1,7 @@
 import { BaseHandler } from "./BaseHandler"
 import { LiquidatePositionsPartyA } from "../../generated/symmio/symmio"
+import { Quote } from "../../generated/schema"
+import { setEventTimestampAndTransactionHashAndAction } from "../utils/quote&analitics&user"
 
 export class LiquidatePositionsPartyAHandler extends BaseHandler {
 	protected event: LiquidatePositionsPartyA
@@ -17,6 +19,17 @@ export class LiquidatePositionsPartyAHandler extends BaseHandler {
 	}
 
 	handleQuote(): void {
-		// TODO
+		const event = this.event
+		for (let i = 0, lenQ = event.params.quoteIds.length; i < lenQ; i++) {
+			let qoutId = event.params.quoteIds[i]
+			let quote = Quote.load(qoutId.toString())!
+			quote.quoteStatus = 8
+			let LiquidateAmount = quote.quantity!.minus(quote.closedAmount!)
+			quote.liquidateAmount = LiquidateAmount
+			quote.save()
+			setEventTimestampAndTransactionHashAndAction(quote.eventsTimestamp, event.block.timestamp,
+				'LiquidatePositionsPartyA', event.transaction.hash, event.block.number)
+
+		}
 	}
 }
