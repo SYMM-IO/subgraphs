ca09467d5af6538f706ec7845d951078f5fb7cff
arrow-wd <arrowd81@proton.me>
2024-07-13 11:31:55 +0200

    added partyAliquidator and SettlePartyALiquidationHandler

--- liquidation/handlers/symmio/LiquidatePendingPositionsPartyAHandler.ts
+++ liquidation/handlers/symmio/LiquidatePendingPositionsPartyAHandler.ts
@@ -2,7 +2,7 @@ import { ethereum } from "@graphprotocol/graph-ts";
 import { Version } from "../../../common/BaseHandler";
 
 import { LiquidatePendingPositionsPartyA } from "../../../generated/symmio_0_8_2/symmio_0_8_2";
-import { update_liquidator } from "./utils";
+import { addLiquidator, update_liquidator } from "./utils";
 
 export class LiquidatePendingPositionsPartyAHandler<T> {
 
@@ -14,6 +14,7 @@ export class LiquidatePendingPositionsPartyAHandler<T> {
             case Version.v_0_8_2: {
                 // @ts-ignore
                 const e = changetype<LiquidatePendingPositionsPartyA>(_event)
+                addLiquidator(e)
             }
             case Version.v_0_8_0: {
 
--- liquidation/src_symmio_0_8_2.ts
+++ liquidation/src_symmio_0_8_2.ts
@@ -1,40 +1,48 @@
 
-import {LiquidatePendingPositionsPartyAHandler} from "./handlers/symmio/LiquidatePendingPositionsPartyAHandler"
-import {LiquidatePendingPositionsPartyA} from "../generated/symmio_0_8_2/symmio_0_8_2"
-
-import {AllocateForPartyBHandler} from "./handlers/symmio/AllocateForPartyBHandler"
-import {AllocateForPartyB} from "../generated/symmio_0_8_2/symmio_0_8_2"
-
-import {LiquidatePositionsPartyBHandler} from "./handlers/symmio/LiquidatePositionsPartyBHandler"
-import {LiquidatePositionsPartyB} from "../generated/symmio_0_8_2/symmio_0_8_2"
-
-import {LiquidatePositionsPartyAHandler} from "./handlers/symmio/LiquidatePositionsPartyAHandler"
-import {LiquidatePositionsPartyA} from "../generated/symmio_0_8_2/symmio_0_8_2"
-
-import {LiquidatePartyBHandler} from "./handlers/symmio/LiquidatePartyBHandler"
-import {LiquidatePartyB} from "../generated/symmio_0_8_2/symmio_0_8_2"
-
 import {AllocatePartyAHandler} from "./handlers/symmio/AllocatePartyAHandler"
 import {AllocatePartyA} from "../generated/symmio_0_8_2/symmio_0_8_2"
 
-import {DeallocatePartyAHandler} from "./handlers/symmio/DeallocatePartyAHandler"
-import {DeallocatePartyA} from "../generated/symmio_0_8_2/symmio_0_8_2"
-
-import {LiquidatePartyAHandler} from "./handlers/symmio/LiquidatePartyAHandler"
-import {LiquidatePartyA} from "../generated/symmio_0_8_2/symmio_0_8_2"
+import {LiquidatePendingPositionsPartyAHandler} from "./handlers/symmio/LiquidatePendingPositionsPartyAHandler"
+import {LiquidatePendingPositionsPartyA} from "../generated/symmio_0_8_2/symmio_0_8_2"
 
 import {DeallocateForPartyBHandler} from "./handlers/symmio/DeallocateForPartyBHandler"
 import {DeallocateForPartyB} from "../generated/symmio_0_8_2/symmio_0_8_2"
 
+import {LiquidatePositionsPartyBHandler} from "./handlers/symmio/LiquidatePositionsPartyBHandler"
+import {LiquidatePositionsPartyB} from "../generated/symmio_0_8_2/symmio_0_8_2"
+
+import {LiquidatePartyBHandler} from "./handlers/symmio/LiquidatePartyBHandler"
+import {LiquidatePartyB} from "../generated/symmio_0_8_2/symmio_0_8_2"
+
+import {LiquidatePartyAHandler} from "./handlers/symmio/LiquidatePartyAHandler"
+import {LiquidatePartyA} from "../generated/symmio_0_8_2/symmio_0_8_2"
+
+import {DeallocatePartyAHandler} from "./handlers/symmio/DeallocatePartyAHandler"
+import {DeallocatePartyA} from "../generated/symmio_0_8_2/symmio_0_8_2"
+
+import {LiquidatePositionsPartyAHandler} from "./handlers/symmio/LiquidatePositionsPartyAHandler"
+import {LiquidatePositionsPartyA} from "../generated/symmio_0_8_2/symmio_0_8_2"
+
+import {SettlePartyALiquidationHandler} from "./handlers/symmio/SettlePartyALiquidationHandler"
+import {SettlePartyALiquidation} from "../generated/symmio_0_8_2/symmio_0_8_2"
+
+import {AllocateForPartyBHandler} from "./handlers/symmio/AllocateForPartyBHandler"
+import {AllocateForPartyB} from "../generated/symmio_0_8_2/symmio_0_8_2"
+
 
 import {Version} from "../common/BaseHandler"
+export function handleAllocatePartyA(event: AllocatePartyA): void {
+    let handler = new AllocatePartyAHandler<AllocatePartyA>()
+    handler.handle(event, Version.v_0_8_2)
+}
+        
 export function handleLiquidatePendingPositionsPartyA(event: LiquidatePendingPositionsPartyA): void {
     let handler = new LiquidatePendingPositionsPartyAHandler<LiquidatePendingPositionsPartyA>()
     handler.handle(event, Version.v_0_8_2)
 }
         
-export function handleAllocateForPartyB(event: AllocateForPartyB): void {
-    let handler = new AllocateForPartyBHandler<AllocateForPartyB>()
+export function handleDeallocateForPartyB(event: DeallocateForPartyB): void {
+    let handler = new DeallocateForPartyBHandler<DeallocateForPartyB>()
     handler.handle(event, Version.v_0_8_2)
 }
         
@@ -43,23 +51,8 @@ export function handleLiquidatePositionsPartyB(event: LiquidatePositionsPartyB):
     handler.handle(event, Version.v_0_8_2)
 }
         
-export function handleLiquidatePositionsPartyA(event: LiquidatePositionsPartyA): void {
-    let handler = new LiquidatePositionsPartyAHandler<LiquidatePositionsPartyA>()
-    handler.handle(event, Version.v_0_8_2)
-}
-        
 export function handleLiquidatePartyB(event: LiquidatePartyB): void {
     let handler = new LiquidatePartyBHandler<LiquidatePartyB>()
-    handler.handle(event, Version.v_0_8_2)
-}
-        
-export function handleAllocatePartyA(event: AllocatePartyA): void {
-    let handler = new AllocatePartyAHandler<AllocatePartyA>()
-    handler.handle(event, Version.v_0_8_2)
-}
-        
-export function handleDeallocatePartyA(event: DeallocatePartyA): void {
-    let handler = new DeallocatePartyAHandler<DeallocatePartyA>()
     handler.handle(event, Version.v_0_8_2)
 }
         
@@ -68,8 +61,23 @@ export function handleLiquidatePartyA(event: LiquidatePartyA): void {
     handler.handle(event, Version.v_0_8_2)
 }
         
-export function handleDeallocateForPartyB(event: DeallocateForPartyB): void {
-    let handler = new DeallocateForPartyBHandler<DeallocateForPartyB>()
+export function handleDeallocatePartyA(event: DeallocatePartyA): void {
+    let handler = new DeallocatePartyAHandler<DeallocatePartyA>()
+    handler.handle(event, Version.v_0_8_2)
+}
+        
+export function handleLiquidatePositionsPartyA(event: LiquidatePositionsPartyA): void {
+    let handler = new LiquidatePositionsPartyAHandler<LiquidatePositionsPartyA>()
+    handler.handle(event, Version.v_0_8_2)
+}
+        
+export function handleSettlePartyALiquidation(event: SettlePartyALiquidation): void {
+    let handler = new SettlePartyALiquidationHandler<SettlePartyALiquidation>()
+    handler.handle(event, Version.v_0_8_2)
+}
+        
+export function handleAllocateForPartyB(event: AllocateForPartyB): void {
+    let handler = new AllocateForPartyBHandler<AllocateForPartyB>()
     handler.handle(event, Version.v_0_8_2)
 }
         
