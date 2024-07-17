2421ef6e3ca36fb6917e8733c5b82379f437c100
kindtechie <kindtechie@gmail.com>
2024-07-17 11:11:27 +0330

    fix liquidation

--- liquidation/handlers/symmio/SettlePartyALiquidationHandler.ts
+++ liquidation/handlers/symmio/SettlePartyALiquidationHandler.ts
@@ -2,7 +2,7 @@ import { ethereum, log } from "@graphprotocol/graph-ts";
 import { Version } from "../../../common/BaseHandler";
 
 import { SettlePartyALiquidation } from "../../../generated/symmio_0_8_2/symmio_0_8_2";
-import { DebugEntity, PartyALiquidator } from "../../../generated/schema";
+import { PartyALiquidator } from "../../../generated/schema";
 import { update_liquidator } from "./utils";
 
 export class SettlePartyALiquidationHandler<T> {
@@ -15,11 +15,7 @@ export class SettlePartyALiquidationHandler<T> {
 				// @ts-ignore
 				const e = changetype<SettlePartyALiquidation>(_event);
 
-				let partyALiquidator = PartyALiquidator.load(e.params.partyA.toHexString());
-				if (!partyALiquidator) {
-					log.error("partyALiquidator not found", []);
-					return;
-				}
+				let partyALiquidator = PartyALiquidator.load(e.params.partyA.toHexString())!;
 				let liquidators = partyALiquidator.Liquidators!;
 				for (let i = 0; i < liquidators.length; i++) {
 					update_liquidator(liquidators[i], e.address)
