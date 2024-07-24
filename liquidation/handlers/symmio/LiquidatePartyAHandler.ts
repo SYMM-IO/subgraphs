eb6b8d01904adc718775011487da3924302ee8eb
kindtechie <kindtechie@gmail.com>
2024-07-24 08:33:55 +0200

    add just 8.2 & 8.0

--- liquidation/handlers/symmio/LiquidatePartyAHandler.ts
+++ liquidation/handlers/symmio/LiquidatePartyAHandler.ts
@@ -5,6 +5,7 @@ import { PartyA } from "../../../generated/schema"
 import { BigInt, ethereum, log } from "@graphprotocol/graph-ts";
 import { Version } from "../../../common/BaseHandler";
 import { getBalanceInfoOfPartyA as getBalanceInfoOfPartyA_0_8_2 } from "../../../common/contract_utils_0_8_2";
+import { getBalanceInfoOfPartyA as getBalanceInfoOfPartyA_0_8_1 } from "../../../common/contract_utils_0_8_1";
 import { getBalanceInfoOfPartyA as getBalanceInfoOfPartyA_0_8_0 } from "../../../common/contract_utils_0_8_0";
 import { LiquidatePartyA } from "../../../generated/symmio_0_8_2/symmio_0_8_2";
 import { addLiquidator } from "./utils";
@@ -18,8 +19,7 @@ export class LiquidatePartyAHandler<T> extends CommonLiquidatePartyAHandler<T> {
 		super.handleGlobalCounter()
 		let entity = PartyA.load(event.params.partyA.toHex())!
 		switch (version) {
-			case Version.v_0_8_2:
-			case Version.v_0_8_1: {
+			case Version.v_0_8_2: {
 				// @ts-ignore
 				const e = changetype<LiquidatePartyA>(_event)
 				const balanceInfoOfPartyA = getBalanceInfoOfPartyA_0_8_2(event.address, event.params.partyA)
@@ -32,11 +32,27 @@ export class LiquidatePartyAHandler<T> extends CommonLiquidatePartyAHandler<T> {
 				entity.liquidateLf = balanceInfoOfPartyA.value2
 				entity.liquidatePendingCva = balanceInfoOfPartyA.value5
 				entity.liquidatePendingLf = balanceInfoOfPartyA.value6
-				if (version == Version.v_0_8_2) {
-					entity.totalUnrealizedLoss = e.params.totalUnrealizedLoss
-					entity.upnl = e.params.upnl
-					entity.allocatedBalance = e.params.allocatedBalance
+
+				entity.totalUnrealizedLoss = e.params.totalUnrealizedLoss
+				entity.upnl = e.params.upnl
+				entity.allocatedBalance = e.params.allocatedBalance
+				addLiquidator(e)
+				break
+				break
+			}
+			case Version.v_0_8_1: {
+				// @ts-ignore
+				const e = changetype<LiquidatePartyA>(_event)
+				const balanceInfoOfPartyA = getBalanceInfoOfPartyA_0_8_1(event.address, event.params.partyA)
+				if (!balanceInfoOfPartyA) {
+					log.error('getBalanceInfoOfPartyA_0_8_1 crashed!', [])
+					return
 				}
+				entity.liquidateAllocatedBalance = balanceInfoOfPartyA.value0
+				entity.liquidateCva = balanceInfoOfPartyA.value1
+				entity.liquidateLf = balanceInfoOfPartyA.value2
+				entity.liquidatePendingCva = balanceInfoOfPartyA.value5
+				entity.liquidatePendingLf = balanceInfoOfPartyA.value6
 				addLiquidator(e)
 				break
 			}
