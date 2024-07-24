bcb8881e35d75e789b2580225c5a3ff3a2080ad9
kindtechie <kindtechie@gmail.com>
2024-07-24 14:50:17 +0200

    fix deposit

--- common/handlers/symmio/DepositHandler.ts
+++ common/handlers/symmio/DepositHandler.ts
@@ -2,16 +2,16 @@ import { BaseHandler, Version } from "../../BaseHandler"
 import { ethereum } from "@graphprotocol/graph-ts/chain/ethereum";
 import { Account, DebugEntity } from "../../../generated/schema";
 import { createNewAccount } from "../../utils/analytics&user_profile";
+import { newUserAndAccount } from "../../../analytics/utils/builders";
 
 export class DepositHandler<T> extends BaseHandler {
 	handleAccount(_event: ethereum.Event, version: Version): void {
 		// @ts-ignore
 		const event = changetype<T>(_event)
 		const globalCounter = super.handleGlobalCounter()
-		let account = Account.load(event.params.user.toHexString())
-		if (!account) {
-			account = createNewAccount(event.params.account, event.params.account, null, event.block, event.transaction, event.params.name)
-		}
+
+		newUserAndAccount(event.params.user, event.block, event.transaction)
+		let account = Account.load(event.params.user.toHexString())!
 		account.deposit = account.deposit.plus(event.params.amount)
 		account.globalCounter = globalCounter
 		account.save()
