8a39dbcc8b2dd13456d65528eba32dce944e89b2
kindtechie <kindtechie@gmail.com>
2024-07-24 14:18:25 +0200

    add 8.3 abi

--- quotes/handlers/symmio/AcceptCancelRequestHandler.ts
+++ quotes/handlers/symmio/AcceptCancelRequestHandler.ts
@@ -1,13 +1,13 @@
 import {
 	AcceptCancelRequestHandler as CommonAcceptCancelRequestHandler
 } from "../../../common/handlers/symmio/AcceptCancelRequestHandler"
-import {getQuote, symbolIdToSymbolName} from "../../../common/utils"
-import {removeQuoteFromPendingList} from "../../../common/utils/quote"
-import {initialQuoteBuilder_0_8_0,} from "../../../common/utils/quote&analitics&user"
+import { getQuote, symbolIdToSymbolName } from "../../../common/contract_utils_0_8_2"
+import { removeQuoteFromPendingList } from "../../../common/utils/quote"
+import { initialQuoteBuilder_0_8_2, } from "../../../common/utils/quote&analitics&user"
 
-import {Quote} from "../../../generated/schema"
-import {ethereum} from "@graphprotocol/graph-ts";
-import {Version} from "../../../common/BaseHandler";
+import { Quote } from "../../../generated/schema"
+import { ethereum, log } from "@graphprotocol/graph-ts";
+import { Version } from "../../../common/BaseHandler";
 
 export class AcceptCancelRequestHandler<T> extends CommonAcceptCancelRequestHandler<T> {
 	handle(_event: ethereum.Event, version: Version): void {
@@ -24,13 +24,17 @@ export class AcceptCancelRequestHandler<T> extends CommonAcceptCancelRequestHand
 			quote.quoteId = event.params.quoteId
 			quote.quoteStatus = event.params.quoteStatus
 
-			const quoteData = getQuote(quote.quoteId, event.address)
-			let initialQuote = initialQuoteBuilder_0_8_0(quoteData)
-			initialQuote.symbol = symbolIdToSymbolName(initialQuote.symbolId, event.address)
-			initialQuote.save()
-			quote.partyA = initialQuote.partyA
-			quote.tradingFee = initialQuote.tradingFee!
-			quote.initialData = initialQuote.id
+			const quoteData = getQuote(event.address, quote.quoteId)
+			if (quoteData) {
+				let initialQuote = initialQuoteBuilder_0_8_2(quoteData)
+				initialQuote.symbol = symbolIdToSymbolName(initialQuote.symbolId, event.address)
+				initialQuote.save()
+				quote.partyA = initialQuote.partyA
+				quote.tradingFee = initialQuote.tradingFee!
+				quote.initialData = initialQuote.id
+			} else {
+				log.error('get quote in quoteid = {} teverted', [quote.quoteId.toString()])
+			}
 			quote.action = 'AcceptCancelRequest'
 			quote.eventsTimestamp = quoteStr
 			quote.transactionsHash = quoteStr
