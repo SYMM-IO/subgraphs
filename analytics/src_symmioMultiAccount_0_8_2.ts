919f9586f842b40c973c76876b1788d692af3989
kindtechie <kindtechie@gmail.com>
2024-07-25 15:46:42 +0330

    add multiaccount on analytics

--- analytics/src_symmioMultiAccount_0_8_2.ts
+++ analytics/src_symmioMultiAccount_0_8_2.ts
@@ -1,11 +1,10 @@
-import {AddAccountHandler} from "./handlers/symmioMultiAccount/AddAccountHandler"
-import {AddAccount} from "../generated/symmioMultiAccount_0_8_2_intentx_multi_account_v2/symmioMultiAccount_0_8_2"
+import { AddAccountHandler } from "./handlers/symmioMultiAccount/AddAccountHandler"
+import { AddAccount } from "../generated/symmioMultiAccount_0_8_2_MultiAccount1/symmioMultiAccount_0_8_2"
 
 
-import {Version} from "../common/BaseHandler"
+import { Version } from "../common/BaseHandler"
 
 export function handleAddAccount(event: AddAccount): void {
 	let handler = new AddAccountHandler<AddAccount>()
 	handler.handle(event, Version.v_0_8_2)
 }
-        
