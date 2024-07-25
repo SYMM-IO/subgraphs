039069291f2c69892840817378a79ace246878af
kindtechie <kindtechie@gmail.com>
2024-07-25 13:13:05 +0200

    add multiaccount's handlers in analytics

--- common/handlers/symmioMultiAccount/AddAccountHandler.ts
+++ common/handlers/symmioMultiAccount/AddAccountHandler.ts
@@ -1,4 +1,18 @@
-import {BaseHandler} from "../../BaseHandler"
+import { ethereum } from "@graphprotocol/graph-ts"
+import { BaseHandler, Version } from "../../BaseHandler"
+import { newUserAndAccount } from "../../../analytics/utils/builders"
+import { Account } from "../../../generated/schema"
 
 export class AddAccountHandler<T> extends BaseHandler {
+    handleAccount(_event: ethereum.Event, version: Version): void {
+        // @ts-ignore
+        const event = changetype<T>(_event)
+        const globalCounter = super.handleGlobalCounter()
+
+        newUserAndAccount(event.params.user, event.block, event.transaction)
+        let account = Account.load(event.params.user.toHexString())!
+        account.deposit = account.deposit.plus(event.params.amount)
+        account.globalCounter = globalCounter
+        account.save()
+    }
 }
