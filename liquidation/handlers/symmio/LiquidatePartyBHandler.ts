2b7e20ac335f45005edd3f9dc1b18967361be3fd
kindtechie <kindtechie@gmail.com>
2024-07-27 13:41:46 +0200

    fix liquidation subgraph

--- liquidation/handlers/symmio/LiquidatePartyBHandler.ts
+++ liquidation/handlers/symmio/LiquidatePartyBHandler.ts
@@ -39,23 +39,19 @@ export class LiquidatePartyBHandler<T> extends CommonLiquidatePartyBHandler<T> {
 				break
 			}
 			case Version.v_0_8_1: {
-				// @ts-ignore
-				// const e = changetype<LiquidatePartyB_0_8_1>(_event)
-				// const balanceInfoOfPartyB = getBalanceInfoOfPartyB_0_8_2(event.address, event.params.partyA, event.params.partyB)
-				// if (!balanceInfoOfPartyB) {
-				// 	log.error('getBalanceInfoOfPartyB_0_8_2 crashed!', [])
-				// 	return
-				// }
-				// entity.liquidateAllocatedBalance = balanceInfoOfPartyB.value0
-				// entity.liquidateCva = balanceInfoOfPartyB.value1
-				// entity.liquidateLf = balanceInfoOfPartyB.value2
-				// entity.liquidatePendingCva = balanceInfoOfPartyB.value5
-				// entity.liquidatePendingLf = balanceInfoOfPartyB.value6
-				// if (version == Version.v_0_8_1) {
-				// 	entity.partyBAllocatedBalance = e.params.partyBAllocatedBalance
-				// 	entity.upnl = e.params.upnl
-				// }
-				// break
+				//@ts-ignore
+				const e = changetype<LiquidatePartyB_0_8_1>(_event)
+				const balanceInfoOfPartyB = getBalanceInfoOfPartyB_0_8_2(event.address, event.params.partyA, event.params.partyB)
+				if (!balanceInfoOfPartyB) {
+					log.error('getBalanceInfoOfPartyB_0_8_2 crashed!', [])
+					return
+				}
+				entity.liquidateAllocatedBalance = balanceInfoOfPartyB.value0
+				entity.liquidateCva = balanceInfoOfPartyB.value1
+				entity.liquidateLf = balanceInfoOfPartyB.value2
+				entity.liquidatePendingCva = balanceInfoOfPartyB.value5
+				entity.liquidatePendingLf = balanceInfoOfPartyB.value6
+				break
 			}
 			case Version.v_0_8_0: {
 				const balanceInfoOfPartyB = getBalanceInfoOfPartyB_0_8_0(event.address, event.params.partyA, event.params.partyB)
---
+++ liquidation/src_symmio_0_8_3.ts
@@ -0,0 +1,115 @@
+import {AllocateForPartyB1} from '../generated/symmio_0_8_3/symmio_0_8_3'
+import {AllocateForPartyBHandler} from './handlers/symmio/AllocateForPartyBHandler'
+import {AllocateForPartyB} from '../generated/symmio_0_8_3/symmio_0_8_3'
+import {AllocatePartyA1} from '../generated/symmio_0_8_3/symmio_0_8_3'
+import {AllocatePartyAHandler} from './handlers/symmio/AllocatePartyAHandler'
+import {AllocatePartyA} from '../generated/symmio_0_8_3/symmio_0_8_3'
+import {DeallocateForPartyB1} from '../generated/symmio_0_8_3/symmio_0_8_3'
+import {DeallocateForPartyBHandler} from './handlers/symmio/DeallocateForPartyBHandler'
+import {DeallocateForPartyB} from '../generated/symmio_0_8_3/symmio_0_8_3'
+import {DeallocatePartyA1} from '../generated/symmio_0_8_3/symmio_0_8_3'
+import {DeallocatePartyAHandler} from './handlers/symmio/DeallocatePartyAHandler'
+import {DeallocatePartyA} from '../generated/symmio_0_8_3/symmio_0_8_3'
+import {LiquidatePartyA1} from '../generated/symmio_0_8_3/symmio_0_8_3'
+import {LiquidatePartyAHandler} from './handlers/symmio/LiquidatePartyAHandler'
+import {LiquidatePartyA} from '../generated/symmio_0_8_3/symmio_0_8_3'
+import {LiquidatePartyBHandler} from './handlers/symmio/LiquidatePartyBHandler'
+import {LiquidatePartyB} from '../generated/symmio_0_8_3/symmio_0_8_3'
+import {SetSymbolsPrices1} from '../generated/symmio_0_8_3/symmio_0_8_3'
+import {SetSymbolsPricesHandler} from './handlers/symmio/SetSymbolsPricesHandler'
+import {SetSymbolsPrices} from '../generated/symmio_0_8_3/symmio_0_8_3'
+import {SettlePartyALiquidation1} from '../generated/symmio_0_8_3/symmio_0_8_3'
+import {SettlePartyALiquidationHandler} from './handlers/symmio/SettlePartyALiquidationHandler'
+import {SettlePartyALiquidation} from '../generated/symmio_0_8_3/symmio_0_8_3'
+import {Version} from '../common/BaseHandler'
+
+
+export function handleSettlePartyALiquidation(event: SettlePartyALiquidation): void {
+    let handler = new SettlePartyALiquidationHandler<SettlePartyALiquidation>()
+    handler.handle(event, Version.v_0_8_3)
+}
+        
+
+export function handleSettlePartyALiquidation1(event: SettlePartyALiquidation1): void {
+    let handler = new SettlePartyALiquidationHandler<SettlePartyALiquidation1>()
+    handler.handle(event, Version.v_0_8_3)
+}
+        
+
+export function handleDeallocatePartyA(event: DeallocatePartyA): void {
+    let handler = new DeallocatePartyAHandler<DeallocatePartyA>()
+    handler.handle(event, Version.v_0_8_3)
+}
+        
+
+export function handleDeallocatePartyA1(event: DeallocatePartyA1): void {
+    let handler = new DeallocatePartyAHandler<DeallocatePartyA1>()
+    handler.handle(event, Version.v_0_8_3)
+}
+        
+
+export function handleLiquidatePartyB(event: LiquidatePartyB): void {
+    let handler = new LiquidatePartyBHandler<LiquidatePartyB>()
+    handler.handle(event, Version.v_0_8_3)
+}
+        
+
+export function handleDeallocateForPartyB(event: DeallocateForPartyB): void {
+    let handler = new DeallocateForPartyBHandler<DeallocateForPartyB>()
+    handler.handle(event, Version.v_0_8_3)
+}
+        
+
+export function handleDeallocateForPartyB1(event: DeallocateForPartyB1): void {
+    let handler = new DeallocateForPartyBHandler<DeallocateForPartyB1>()
+    handler.handle(event, Version.v_0_8_3)
+}
+        
+
+export function handleLiquidatePartyA(event: LiquidatePartyA): void {
+    let handler = new LiquidatePartyAHandler<LiquidatePartyA>()
+    handler.handle(event, Version.v_0_8_3)
+}
+        
+
+export function handleLiquidatePartyA1(event: LiquidatePartyA1): void {
+    let handler = new LiquidatePartyAHandler<LiquidatePartyA1>()
+    handler.handle(event, Version.v_0_8_3)
+}
+        
+
+export function handleSetSymbolsPrices(event: SetSymbolsPrices): void {
+    let handler = new SetSymbolsPricesHandler<SetSymbolsPrices>()
+    handler.handle(event, Version.v_0_8_3)
+}
+        
+
+export function handleSetSymbolsPrices1(event: SetSymbolsPrices1): void {
+    let handler = new SetSymbolsPricesHandler<SetSymbolsPrices1>()
+    handler.handle(event, Version.v_0_8_3)
+}
+        
+
+export function handleAllocateForPartyB(event: AllocateForPartyB): void {
+    let handler = new AllocateForPartyBHandler<AllocateForPartyB>()
+    handler.handle(event, Version.v_0_8_3)
+}
+        
+
+export function handleAllocateForPartyB1(event: AllocateForPartyB1): void {
+    let handler = new AllocateForPartyBHandler<AllocateForPartyB1>()
+    handler.handle(event, Version.v_0_8_3)
+}
+        
+
+export function handleAllocatePartyA(event: AllocatePartyA): void {
+    let handler = new AllocatePartyAHandler<AllocatePartyA>()
+    handler.handle(event, Version.v_0_8_3)
+}
+        
+
+export function handleAllocatePartyA1(event: AllocatePartyA1): void {
+    let handler = new AllocatePartyAHandler<AllocatePartyA1>()
+    handler.handle(event, Version.v_0_8_3)
+}
+        
