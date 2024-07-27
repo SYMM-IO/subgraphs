7fc04cb8a6b6438c508af3311aa0f9db333c7136
kindtechie <kindtechie@gmail.com>
2024-07-27 13:30:03 +0200

    fix add account handler on multiaccount

--- common/handlers/symmioMultiAccount/AddAccountHandler.ts
+++ common/handlers/symmioMultiAccount/AddAccountHandler.ts
@@ -1,18 +1,24 @@
-import { ethereum } from "@graphprotocol/graph-ts"
+import { BigInt, ethereum } from "@graphprotocol/graph-ts"
 import { BaseHandler, Version } from "../../BaseHandler"
-import { newUserAndAccount } from "../../../analytics/utils/builders"
-import { Account } from "../../../generated/schema"
+import { getDailyHistoryForTimestamp, getTotalHistory, newUserAndAccount } from "../../../analytics/utils/builders"
+import { Account, User } from "../../../generated/schema"
+import { createNewAccount, createNewUser } from "../../utils/analytics&user_profile"
 
 export class AddAccountHandler<T> extends BaseHandler {
     handleAccount(_event: ethereum.Event, version: Version): void {
         // @ts-ignore
         const event = changetype<T>(_event)
-        const globalCounter = super.handleGlobalCounter()
-
-        newUserAndAccount(event.params.user, event.block, event.transaction)
-        let account = Account.load(event.params.user.toHexString())!
-        account.deposit = account.deposit.plus(event.params.amount)
-        account.globalCounter = globalCounter
-        account.save()
+        let user = User.load(event.params.user.toHexString())
+        if (user == null) {
+            user = createNewUser(event.params.user, event.block, event.transaction)
+        } else {
+            const dh = getDailyHistoryForTimestamp(event.block.timestamp, event.address)
+            dh.newUsers = dh.newUsers.plus(BigInt.fromString("1"))
+            dh.save()
+            const th = getTotalHistory(event.block.timestamp, event.address)
+            th.users = th.users.plus(BigInt.fromString("1"))
+            th.save()
+        }
+        createNewAccount(event.params.account, user, event.address, event.block, event.transaction, event.params.name)
     }
 }
