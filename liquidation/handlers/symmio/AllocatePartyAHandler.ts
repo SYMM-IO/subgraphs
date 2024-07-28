b026d33544f59d3a0e91a535ff481d924cc7c636
kindtechie <kindtechie@gmail.com>
2024-07-28 14:20:48 +0200

    update liquidation allocate partyA to 8.3

--- liquidation/handlers/symmio/AllocatePartyAHandler.ts
+++ liquidation/handlers/symmio/AllocatePartyAHandler.ts
@@ -1,10 +1,13 @@
 import {
 	AllocatePartyAHandler as CommonAllocatePartyAHandler
 } from "../../../common/handlers/symmio/AllocatePartyAHandler"
-import {getGlobalCounterAndInc} from "../../../common/utils"
+import { getGlobalCounterAndInc } from "../../../common/utils"
 import { Liquidator, PartyA } from "../../../generated/schema";
-import {ethereum} from "@graphprotocol/graph-ts";
-import {Version} from "../../../common/BaseHandler";
+import { ethereum } from "@graphprotocol/graph-ts";
+import { Version } from "../../../common/BaseHandler";
+import { AllocatePartyA as AllocatePartyA_8_2 } from "../../../generated/symmio_0_8_2/symmio_0_8_2";
+import { AllocatePartyA as AllocatePartyA_8_3 } from "../../../generated/symmio_0_8_3/symmio_0_8_3";
+
 
 export class AllocatePartyAHandler<T> extends CommonAllocatePartyAHandler<T> {
 
@@ -16,7 +19,15 @@ export class AllocatePartyAHandler<T> extends CommonAllocatePartyAHandler<T> {
 		let allocateEntity = PartyA.load(event.params.user.toHex())
 		let globalCounter = getGlobalCounterAndInc()
 		if (allocateEntity) {
-			allocateEntity.amount = allocateEntity.amount.plus(event.params.amount)
+			if (version == Version.v_0_8_2) {
+				// @ts-ignore
+				const e = changetype<AllocatePartyA_8_2>(_event)
+				allocateEntity.amount = allocateEntity.amount.plus(e.params.amount)
+			} else if (version == Version.v_0_8_3) {
+				// @ts-ignore
+				const e = changetype<AllocatePartyA_8_3>(_event)
+				allocateEntity.amount = event.params.newAllocatedBalance
+			}
 		} else {
 			allocateEntity = new PartyA(event.params.user.toHex())
 			allocateEntity.index = globalCounter
@@ -30,6 +41,15 @@ export class AllocatePartyAHandler<T> extends CommonAllocatePartyAHandler<T> {
 		allocateEntity.save()
 		let liquidator = Liquidator.load(event.params.user.toHexString());
 		if (liquidator) {
+			if (version == Version.v_0_8_2) {
+				// @ts-ignore
+				const e = changetype<AllocatePartyA_8_2>(_event)
+				liquidator.balance = liquidator.balance.plus(event.params.amount)
+			} else if (version == Version.v_0_8_3) {
+				// @ts-ignore
+				const e = changetype<AllocatePartyA_8_3>(_event)
+				liquidator.balance = event.params.newAllocatedBalance
+			}
 			liquidator.balance = liquidator.balance.plus(event.params.amount)
 			liquidator.save()
 		}
--- liquidation/src_symmio_0_8_3.ts
+++ liquidation/src_symmio_0_8_3.ts
@@ -12,20 +12,13 @@ import {LiquidatePartyBHandler} from './handlers/symmio/LiquidatePartyBHandler'
 import {LiquidatePartyB} from '../generated/symmio_0_8_3/symmio_0_8_3'
 import {SetSymbolsPricesHandler} from './handlers/symmio/SetSymbolsPricesHandler'
 import {SetSymbolsPrices} from '../generated/symmio_0_8_3/symmio_0_8_3'
-import {SettlePartyALiquidation1} from '../generated/symmio_0_8_3/symmio_0_8_3'
 import {SettlePartyALiquidationHandler} from './handlers/symmio/SettlePartyALiquidationHandler'
 import {SettlePartyALiquidation} from '../generated/symmio_0_8_3/symmio_0_8_3'
 import {Version} from '../common/BaseHandler'
 
 
-export function handleDeallocateForPartyB(event: DeallocateForPartyB): void {
-    let handler = new DeallocateForPartyBHandler<DeallocateForPartyB>()
-    handler.handle(event, Version.v_0_8_3)
-}
-        
-
-export function handleLiquidatePartyB(event: LiquidatePartyB): void {
-    let handler = new LiquidatePartyBHandler<LiquidatePartyB>()
+export function handleSetSymbolsPrices(event: SetSymbolsPrices): void {
+    let handler = new SetSymbolsPricesHandler<SetSymbolsPrices>()
     handler.handle(event, Version.v_0_8_3)
 }
         
@@ -36,8 +29,14 @@ export function handleSettlePartyALiquidation(event: SettlePartyALiquidation): v
 }
         
 
-export function handleSettlePartyALiquidation1(event: SettlePartyALiquidation1): void {
-    let handler = new SettlePartyALiquidationHandler<SettlePartyALiquidation1>()
+export function handleDeallocatePartyA(event: DeallocatePartyA): void {
+    let handler = new DeallocatePartyAHandler<DeallocatePartyA>()
+    handler.handle(event, Version.v_0_8_3)
+}
+        
+
+export function handleAllocatePartyA(event: AllocatePartyA): void {
+    let handler = new AllocatePartyAHandler<AllocatePartyA>()
     handler.handle(event, Version.v_0_8_3)
 }
         
@@ -48,8 +47,8 @@ export function handleAllocateForPartyB(event: AllocateForPartyB): void {
 }
         
 
-export function handleDeallocatePartyA(event: DeallocatePartyA): void {
-    let handler = new DeallocatePartyAHandler<DeallocatePartyA>()
+export function handleDeallocateForPartyB(event: DeallocateForPartyB): void {
+    let handler = new DeallocateForPartyBHandler<DeallocateForPartyB>()
     handler.handle(event, Version.v_0_8_3)
 }
         
@@ -60,14 +59,8 @@ export function handleLiquidatePartyA(event: LiquidatePartyA): void {
 }
         
 
-export function handleSetSymbolsPrices(event: SetSymbolsPrices): void {
-    let handler = new SetSymbolsPricesHandler<SetSymbolsPrices>()
-    handler.handle(event, Version.v_0_8_3)
-}
-        
-
-export function handleAllocatePartyA(event: AllocatePartyA): void {
-    let handler = new AllocatePartyAHandler<AllocatePartyA>()
+export function handleLiquidatePartyB(event: LiquidatePartyB): void {
+    let handler = new LiquidatePartyBHandler<LiquidatePartyB>()
     handler.handle(event, Version.v_0_8_3)
 }
         
