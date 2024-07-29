3b3febe33127f08a853681978761b02de2754724
kindtechie <kindtechie@gmail.com>
2024-07-29 10:55:37 +0330

    add new allocate balance to liquidation

--- liquidation/handlers/symmio/AllocateForPartyBHandler.ts
+++ liquidation/handlers/symmio/AllocateForPartyBHandler.ts
@@ -1,9 +1,11 @@
 import {
 	AllocateForPartyBHandler as CommonAllocateForPartyBHandler
 } from "../../../common/handlers/symmio/AllocateForPartyBHandler"
-import {PartyB} from "../../../generated/schema"
-import {ethereum} from "@graphprotocol/graph-ts";
-import {Version} from "../../../common/BaseHandler";
+import { PartyB } from "../../../generated/schema"
+import { BigInt, ethereum } from "@graphprotocol/graph-ts";
+import { Version } from "../../../common/BaseHandler";
+import { AllocateForPartyB as AllocateForPartyB_8_3 } from "../../../generated/symmio_0_8_3/symmio_0_8_3";
+import { AllocateForPartyB as AllocateForPartyB_8_2 } from "../../../generated/symmio_0_8_2/symmio_0_8_2";
 
 export class AllocateForPartyBHandler<T> extends CommonAllocateForPartyBHandler<T> {
 
@@ -12,13 +14,26 @@ export class AllocateForPartyBHandler<T> extends CommonAllocateForPartyBHandler<
 		const event = changetype<T>(_event)
 		super.handle(_event, version)
 		const globalCounter = super.handleGlobalCounter()
+
+		const isVersion_8_3 = version == Version.v_0_8_3
+		let eventAmount: BigInt
+		if (isVersion_8_3) {
+			// @ts-ignore
+			let e = changetype<AllocateForPartyB_8_3>(_event)
+			eventAmount = e.params.newAllocatedBalance
+		} else {
+			// @ts-ignore
+			let e = changetype<AllocateForPartyB_8_2>(_event)
+			eventAmount = e.params.amount
+		}
+
 		let allocateEntity = PartyB.load(event.params.partyA.toHex() + '-' + event.params.partyB.toHex())
 		if (allocateEntity) {
-			allocateEntity.amount = allocateEntity.amount.plus(event.params.amount)
+			allocateEntity.amount = isVersion_8_3 ? eventAmount : allocateEntity.amount.plus(eventAmount)
 		} else {
 			allocateEntity = new PartyB(event.params.partyA.toHex() + '-' + event.params.partyB.toHex())
 			allocateEntity.index = globalCounter
-			allocateEntity.amount = event.params.amount
+			allocateEntity.amount = eventAmount
 			allocateEntity.partyA = event.params.partyA
 			allocateEntity.partyB = event.params.partyB
 		}
