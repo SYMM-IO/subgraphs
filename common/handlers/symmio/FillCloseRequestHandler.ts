121f9aae324f795131ed74e1053102e208cdf174
kindtechie <kindtechie@gmail.com>
2024-07-29 08:06:35 +0200

    add debug on analytics

--- common/handlers/symmio/FillCloseRequestHandler.ts
+++ common/handlers/symmio/FillCloseRequestHandler.ts
@@ -1,20 +1,31 @@
-import {Quote} from "../../../generated/schema"
-import {setEventTimestampAndTransactionHashAndAction} from "../../utils/quote&analitics&user"
-import {BaseHandler, Version} from "../../BaseHandler"
-import {ethereum} from "@graphprotocol/graph-ts";
-import {getQuote as getQuote_0_8_2} from "../../../common/contract_utils_0_8_2";
-import {getQuote as getQuote_0_8_0} from "../../../common/contract_utils_0_8_0";
+import { DebugEntity, Quote } from "../../../generated/schema"
+import { setEventTimestampAndTransactionHashAndAction } from "../../utils/quote&analitics&user"
+import { BaseHandler, Version } from "../../BaseHandler"
+import { ethereum } from "@graphprotocol/graph-ts";
+import { getQuote as getQuote_0_8_2 } from "../../../common/contract_utils_0_8_2";
+import { getQuote as getQuote_0_8_0 } from "../../../common/contract_utils_0_8_0";
 
 export class FillCloseRequestHandler<T> extends BaseHandler {
 	handleQuote(_event: ethereum.Event, version: Version): void {
+		let db = new DebugEntity("FillCloseRequestHandler common")
 		// @ts-ignore
 		const event = changetype<T>(_event)
-		let quote = Quote.load(event.params.quoteId.toString())!
+		let quote = Quote.load(event.params.quoteId.toString())
+		if (!quote) { //  TODO: remove after debug
+			db.message = `quoteId: ${event.params.quoteId.toString()} not exist`
+			db.save()
+			return
+		}
 		quote.globalCounter = super.handleGlobalCounter()
 
 		switch (version) {
 			case Version.v_0_8_2: {
-				let q = getQuote_0_8_2(event.address, event.params.quoteId)!
+				let q = getQuote_0_8_2(event.address, event.params.quoteId)
+				if (!q) {
+					db.message = `quoteId: ${event.params.quoteId.toString()} getQuote_0_8_2 problem`
+					db.save()
+					return
+				}
 				quote.cva = q.lockedValues.cva
 				quote.partyAmm = q.lockedValues.partyAmm
 				quote.partyBmm = q.lockedValues.partyBmm
@@ -22,7 +33,12 @@ export class FillCloseRequestHandler<T> extends BaseHandler {
 				break
 			}
 			case Version.v_0_8_0: {
-				let q = getQuote_0_8_0(event.address, event.params.quoteId)!
+				let q = getQuote_0_8_0(event.address, event.params.quoteId)
+				if (!q) {
+					db.message = `quoteId: ${event.params.quoteId.toString()} getQuote_0_8_0 problem`
+					db.save()
+					return
+				}
 				quote.cva = q.lockedValues.cva
 				quote.partyAmm = q.lockedValues.mm
 				quote.partyBmm = q.lockedValues.mm
