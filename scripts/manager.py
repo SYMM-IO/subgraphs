d451a8b79265cf4b777fc201fbe3d003a0d82dab
naveed <naveedinno@proton.me>
2024-07-31 12:04:17 +0200

    Add vaults versions to manager script

--- scripts/manager.py
+++ scripts/manager.py
@@ -49,7 +49,9 @@ class Config:
 
 abi_versions = {
     "symmio": ["0_8_0", "0_8_2", "0_8_3"],
-    "symmioMultiAccount": ["0_8_0", "0_8_2"]
+    "symmioMultiAccount": ["0_8_0", "0_8_2"],
+    "off_chain_vault": ["1"],
+    "on_chain_vault": ["1"]
 }
 
 
