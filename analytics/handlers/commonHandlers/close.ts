ad5f422cad6a4f3404b433156fc1a3ecb57dab03
kindtechie <kindtechie@gmail.com>
2024-07-31 09:34:50 +0330

    add logs

--- analytics/handlers/commonHandlers/close.ts
+++ analytics/handlers/commonHandlers/close.ts
@@ -1,13 +1,17 @@
-import {ethereum} from "@graphprotocol/graph-ts/chain/ethereum";
-import {Account, PriceCheck, Quote, Symbol, TradeHistory} from "../../../generated/schema";
-import {BigInt} from "@graphprotocol/graph-ts";
-import {unDecimal, updateDailyOpenInterest, updateHistories, UpdateHistoriesParams} from "../../utils/helpers";
-import {PRICE_CHECK} from "../../config";
+import { ethereum } from "@graphprotocol/graph-ts/chain/ethereum";
+import { Account, PriceCheck, Quote, Symbol, TradeHistory } from "../../../generated/schema";
+import { BigInt, log } from "@graphprotocol/graph-ts";
+import { unDecimal, updateDailyOpenInterest, updateHistories, UpdateHistoriesParams } from "../../utils/helpers";
+import { PRICE_CHECK } from "../../config";
 
 export function handleClose<T>(_event: ethereum.Event, name: string): void {
 	// @ts-ignore
 	const event = changetype<T>(_event) // FillClose, ForceClose, EmergencyClose all have the same event signature
-	let quote = Quote.load(event.params.quoteId.toString())!
+	let quote = Quote.load(event.params.quoteId.toString())
+	if (!quote) {
+		log.debug('quote not exist. quoteId {}', [event.params.quoteId.toString()])
+		return
+	}
 	let history = TradeHistory.load(event.params.partyA.toHexString() + "-" + event.params.quoteId.toString())!
 	const additionalVolume = event.params.filledAmount.times(event.params.closedPrice).div(BigInt.fromString("10").pow(18))
 	history.volume = history.volume.plus(additionalVolume)
@@ -31,8 +35,8 @@ export function handleClose<T>(_event: ethereum.Event, name: string): void {
 
 	const pnl = unDecimal(
 		(quote.positionType == 0
-				? BigInt.fromString("1")
-				: BigInt.fromString("1").neg()
+			? BigInt.fromString("1")
+			: BigInt.fromString("1").neg()
 		)
 			.times(event.params.closedPrice.minus(quote.openedPrice!))
 			.times(event.params.filledAmount),
