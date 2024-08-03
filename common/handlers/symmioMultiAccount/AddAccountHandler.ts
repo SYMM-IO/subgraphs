e924aeda93c5e1639cf346716e33066c5a976eb6
naveed <naveedinno@proton.me>
2024-08-03 17:16:29 +0200

    Fix users/accounts historical data

--- common/handlers/symmioMultiAccount/AddAccountHandler.ts
+++ common/handlers/symmioMultiAccount/AddAccountHandler.ts
@@ -1,24 +1,25 @@
-import { BigInt, ethereum } from "@graphprotocol/graph-ts"
-import { BaseHandler, Version } from "../../BaseHandler"
-import { getDailyHistoryForTimestamp, getTotalHistory, newUserAndAccount } from "../../../analytics/utils/builders"
-import { Account, User } from "../../../generated/schema"
-import { createNewAccount, createNewUser } from "../../utils/analytics&user_profile"
+import {BigInt, ethereum} from "@graphprotocol/graph-ts"
+import {BaseHandler, Version} from "../../BaseHandler"
+import {getDailyHistoryForTimestamp, getTotalHistory} from "../../../analytics/utils/builders"
+import {User} from "../../../generated/schema"
+import {createNewAccount, createNewUser} from "../../utils/analytics&user_profile"
 
 export class AddAccountHandler<T> extends BaseHandler {
-    handleAccount(_event: ethereum.Event, version: Version): void {
-        // @ts-ignore
-        const event = changetype<T>(_event)
-        let user = User.load(event.params.user.toHexString())
-        if (user == null) {
-            user = createNewUser(event.params.user, event.block, event.transaction)
-        } else {
-            const dh = getDailyHistoryForTimestamp(event.block.timestamp, event.address)
-            dh.newUsers = dh.newUsers.plus(BigInt.fromString("1"))
-            dh.save()
-            const th = getTotalHistory(event.block.timestamp, event.address)
-            th.users = th.users.plus(BigInt.fromString("1"))
-            th.save()
-        }
-        createNewAccount(event.params.account, user, event.address, event.block, event.transaction, event.params.name)
-    }
+	handleAccount(_event: ethereum.Event, version: Version): void {
+		// @ts-ignore
+		const event = changetype<T>(_event)
+		const dh = getDailyHistoryForTimestamp(event.block.timestamp, event.address)
+		const th = getTotalHistory(event.block.timestamp, event.address)
+		let user = User.load(event.params.user.toHexString())
+		if (user == null) {
+			user = createNewUser(event.params.user, event.block, event.transaction)
+			dh.newUsers = dh.newUsers.plus(BigInt.fromString("1"))
+			th.users = th.users.plus(BigInt.fromString("1"))
+		}
+		createNewAccount(event.params.account, user, event.address, event.block, event.transaction, event.params.name)
+		dh.newAccounts = dh.newAccounts.plus(BigInt.fromString("1"))
+		th.accounts = th.accounts.plus(BigInt.fromString("1"))
+		dh.save()
+		th.save()
+	}
 }
