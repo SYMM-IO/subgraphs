f0159d8c7ccdef4f8b8eba860f89448d1879756b
naveed <naveedinno@proton.me>
2024-08-07 11:22:51 +0200

    Fix tradingFee problem

--- analytics/handlers/symmio/SetSymbolTradingFeeHandler.ts
+++ analytics/handlers/symmio/SetSymbolTradingFeeHandler.ts
@@ -5,7 +5,10 @@ import {
 import {Version} from "../../../common/BaseHandler";
 
 export class SetSymbolTradingFeeHandler<T> extends CommonSetSymbolTradingFeeHandler<T> {
-	handleSymbol(_event: ethereum.Event, version: Version): void {
+	handle(_event: ethereum.Event, version: Version): void {
+		super.handle(_event, version)
+		super.handleQuote(_event, version)
 		super.handleSymbol(_event, version)
+		super.handleAccount(_event, version)
 	}
 }
