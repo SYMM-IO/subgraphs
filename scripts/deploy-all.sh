db5aaa1bddd8528abcf3659e832513f0f04626fa
kindtechie <kindtechie@gmail.com>
2024-12-24 13:27:23 +0200

    fix python script name

--- scripts/deploy-all.sh
+++ scripts/deploy-all.sh
@@ -2,7 +2,7 @@
 prefix="${1:-symmio}"
 for proj in analytics events
 do
-	python3 manager.py configs/docker.json $proj
+	python3 scripts/manager.py configs/docker.json $proj
 	cd $proj
 	echo "********** processing $proj **********"
 	../manage -cbd $prefix-$proj
