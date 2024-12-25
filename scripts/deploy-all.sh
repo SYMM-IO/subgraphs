73f5fd60555a9458e204874c09154fb1e1861d66
kindtechie <kindtechie@gmail.com>
2024-12-25 06:26:45 +0200

    fix docker deployment shell script

--- scripts/deploy-all.sh
+++ scripts/deploy-all.sh
@@ -3,7 +3,7 @@ prefix="${1:-symmio}"
 for proj in analytics events
 do
 	python3 scripts/manager.py configs/docker.json $proj
-	cd $proj
+	# cd $proj
 	echo "********** processing $proj **********"
 	../scripts/manage -cbd $prefix-$proj
 	cd ..
