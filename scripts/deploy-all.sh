d8c43726c20b0da352b7d24169741ef02270e54a
naveed <naveedinno@proton.me>
2025-04-03 14:02:38 +0200

    Fix minor problem

--- scripts/deploy-all.sh
+++ scripts/deploy-all.sh
@@ -1,10 +1,9 @@
 #!/bin/bash -ex
 prefix="${1:-symmio}"
-for proj in perps/analytics perps/events
-do
-	python3 scripts/manager.py configs/perps/docker.json $proj
+for proj in analytics events; do
+	python3 scripts/manager.py configs/perps/docker.json perps/$proj
 	# cd $proj
-	echo "********** processing $proj **********"
-	./scripts/manage -cbd $prefix-$proj
+	echo "********** processing perps/$proj **********"
+	./scripts/manage -cbd $prefix-perps/$proj
 	# cd ..
 done
