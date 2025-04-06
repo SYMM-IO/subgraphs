96b320de1c07b49dac02fbe86e2dbca7eb7c7843
naveed <naveedinno@proton.me>
2025-04-06 08:50:42 +0200

    Fix minor problem

--- scripts/deploy-all.sh
+++ scripts/deploy-all.sh
@@ -4,6 +4,6 @@ for proj in analytics events; do
 	python3 scripts/manager.py configs/perps/docker.json perps/$proj
 	# cd $proj
 	echo "********** processing perps/$proj **********"
-	./scripts/manage -d $prefix/$proj
+	./scripts/manage -d $prefix-$proj
 	# cd ..
 done
