b6680535f903cb11d46d85041e1afdca5b0984b9
timaster <timaster27@proton.me>
2025-08-02 23:33:06 +0200

    Fix AddAccount

--- perps/events/handlers/symmioMultiAccount/AddAccountHandler.ts
+++ perps/events/handlers/symmioMultiAccount/AddAccountHandler.ts
@@ -7,7 +7,7 @@ export class AddAccountHandler<T> {
 	handle(_event: ethereum.Event, version: MultiAccountVersion): void {
 		// @ts-ignore
 		const event = changetype<T>(_event)
-		let entity = new AddAccount(event.params.account.toString())
+		let entity = new AddAccount(event.params.account.toHexString())
 		entity.user = event.params.user
 		entity.counterId = getGlobalCounterAndInc()
 		entity.account = event.params.account
