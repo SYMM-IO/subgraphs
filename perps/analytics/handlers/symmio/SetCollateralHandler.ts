8cc7446004f6cffba6fbaf6eb560d3c7b72fd27c
naveed <naveedinno@proton.me>
2026-02-23 07:06:14 +0100

    Fix SetCollateralHandler to restore Configuration entity creation

--- perps/analytics/handlers/symmio/SetCollateralHandler.ts
+++ perps/analytics/handlers/symmio/SetCollateralHandler.ts
@@ -2,12 +2,15 @@
 import { SetCollateralHandler as CommonSetCollateralHandler } from "../../../common/handlers/symmio/SetCollateralHandler"
 import {ethereum} from "@graphprotocol/graph-ts";
 import {Version} from "../../../common/BaseHandler";
+import {getConfiguration} from "../../utils/builders";
 
 export class SetCollateralHandler<T> extends CommonSetCollateralHandler<T> {
     handle(_event: ethereum.Event, version: Version): void {
         // @ts-ignore
         const event = changetype<T>(_event)
         super.handle(_event, version)
-
+        let configuration = getConfiguration(event)
+        configuration.collateral = event.params.collateral
+        configuration.save()
     }
 }
